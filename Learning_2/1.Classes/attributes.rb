76c0b72d63e0de6549826f6bcbcfd8897910fc9c
Daniel Villarreal <dvillarreal93@gmail.com>
2021-12-15 14:54:22 -0700

    Added attributes for Classes in Ruby

---
+++ Learning_2/1.Classes/attributes.rb
@@ -0,0 +1,24 @@
+=begin
+    Attributes
+    Also known as properties, these are values that identify 
+    certain type of objects
+
+    * Properties of an object
+    * They are preceeded by an @ sign
+        @variable
+    * An instance of a class always has access to its instance variables (inside the class)
+    * Instance are accessible from outside an instance
+    * Use methods to access instance variables (Reader/Writer methods, or getter/Setter methods)
+    * Ruby has a convention for naming them
+
+    --> Get
+    def noise
+        @noise
+    end
+
+    --> Set
+    def noise=(value)
+        @noise = value
+    end
+
+=end
--- Learning_2/ruby_project/init.rb
+++ Learning_2/ruby_project/init.rb
@@ -2,6 +2,11 @@
 
 # imports
 require_relative 'classes/person'
+require_relative 'classes/animal'
 
-person = Person.new
-puts person.say_hello
+# person = Person.new
+# puts person.say_hello
+
+pig = Animal.new
+pig.noise= 'Oink!'
+puts pig.noise
