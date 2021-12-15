ce91f95b39c2489a5df0fe4710e04787256b5332
Daniel Villarreal <dvillarreal93@gmail.com>
2021-12-15 15:12:24 -0700

    Looking further into Attribute methods

--- Learning_2/1.Classes/attributes.rb
+++ Learning_2/1.Classes/attributes.rb
@@ -21,4 +21,43 @@
         @noise = value
     end
 
+    ------------------------------------------------------------------------------------------------------------
+    Attribute Methods
+    Also known as attr_* methods
+    * attr_reader
+        attr_reader :name
+    --> same as...
+        def name
+            @name
+        end
+
+    * attr_writer
+        attr_writer :name
+    --> same as...
+        def name=(value)
+            @name = value
+        end
+
+    * attr_accessor
+        attr_accesor :name
+    
+    --> same as...
+    def name
+        @name
+    end
+
+    def name=(value)
+        @name = value
+    end
+
+    ----
+
+    Resume:
+    * Use self to reference the current instance from code inside the instance
+    * Add self when calling writer methods (self.first_name=)
+    * Helps Ruby understand to call the method
+    * Omit self when calling any other method (first_name)
+    * Including self is always the safest choice
+
+    ------------------------------------------------------------------------------------------------------------
 =end
--- Learning_2/ruby_project/classes/animal.rb
+++ Learning_2/ruby_project/classes/animal.rb
@@ -1,9 +1,14 @@
 class Animal
-    def noise
-        @noise
-    end
+    attr_accessor :noise, :color
+    # attr_reader :noise
+    # attr_writer :noise
+    
+    # same as:
+    # def noise
+    #     @noise
+    # end
 
-    def noise=(value)
-        @noise = value
-    end
+    # def noise=(value)
+    #     @noise = value
+    # end
 end
