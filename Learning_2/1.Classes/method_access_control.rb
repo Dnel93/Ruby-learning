1c9a4fb6c5d908b794950c4f1c1f48c6c8876f4d
Daniel Villarreal <dvillarreal93@gmail.com>
2021-12-15 15:22:24 -0700

    Method access control

---
+++ Learning_2/1.Classes/method_access_control.rb
@@ -0,0 +1,18 @@
+=begin
+    Method Access Control
+    * Methods are the primary interfaces to a class
+    * Only expose interfaces when necessary
+    * Access control restricts access to methods from outside an instance
+
+    Method Access Control Types:
+    * public: anyone can access (default)
+    * protected: can only be called by instances of the class and its subclasses
+    * private: can only be called by instances of a class
+
+    private # --> All the methods below will be private
+        ...
+    
+    protected # --> All the methods below will be protected
+        ...
+    
+=end
